9045d5cf22017c7b36d16d9a0855a6da0e19e318
Chibyke98 <chibuikemnwachukwu@gmail.com>
2022-09-26 04:15:58 -0700

    Task 0 done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _memcpy - Fills a buffer with a constant for a specified size
- * @dest: This is the buffer to be filled
- * @src: This is the char that will be used to fill buffer
- * @n: This is the no of bytes to fill in the memory array of dest
- *
- * Return: returns dest
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		s[i] = b;
 	}
-
-	return (dest);
+	return (s);
 }
